21639eb2b2fb625a5942ad008ec0378020388f4d
Himanshu Singh <bt20cse062@sce.org.in>
2021-08-12 10:17:13 +0530

    Update LargenNumber.java

--- eclipse-workspace/Introduction/src/function/LargenNumber.java
+++ eclipse-workspace/Introduction/src/function/LargenNumber.java
@@ -2,22 +2,15 @@ package function;
 
 public class LargenNumber {
 
-	
-	
-	public static void main(String[] args) {
+public static void main(String[] args) {
      int s = 10;
      int m = 21;
      int ans=sum(s,m);
      System.out.println(ans);
 
-
-
-	}
-	
-	
+}
 	
  static int sum(int a ,int b) {
-	
 	if(a>b) {
 		return a;
 	}else {
